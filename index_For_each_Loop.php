976aca211a2a9c8d627518069b61f15cd5fd985b
Tarrzuhri <tarrzuhri@gmail.com>
2023-05-31 12:21:33 +0700

    struktur

---
+++ index_For_each_Loop.php
@@ -0,0 +1,18 @@
+<?php
+$nama = array(“Adi”, “Budi”, “Carli”, “Dedy”);
+foreach($nama as $val){
+echo “$val”;
+echo “<br />”;
+}
+?> 
+
+
+
+<!--menggunakan for-->
+<?php
+$nama = array(“Adi”, “Budi”, “Carli”, “Dedy”);
+for($i=0; $i<4; $i++){
+echo “$nama[$i]”;
+echo “<br />”;
+}
+?> 
